ef2c2bdba51ce88299ccac2ad6a0f2a6180719e1
Hogen <dannyliesco@gmail.com>
2021-01-26 18:52:52 +0800

    this is first time to commit.

---
+++ Tavern.kt
@@ -0,0 +1,19 @@
+const val TAVERN_NAME = "Taernyl's Folly"
+
+var playerGold = 10
+var playerSilver = 10
+
+fun main() {
+
+}
+
+fun performPurchase(){
+    displayBalance()
+}
+
+private fun displayBalance(){
+    println("Player's purse balance: Gold: $playerGold , Silver: $playerSilver")
+}
+
+
+
