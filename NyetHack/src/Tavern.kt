fc0904533408054f77b115a94b6ef7b97d0d4792
hogen <dannyliesco@gmail.com>
2021-01-30 07:03:08 +0800

    第十章 List与Set

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -1,36 +1,55 @@
-import com.sun.webkit.event.WCInputMethodEvent
-import kotlin.math.roundToInt
+import com.sun.xml.internal.fastinfoset.util.StringArray
+import java.io.File
 
 const val TAVERN_NAME = "Taernyl's Folly"
 const val WINE_GALLON = 5
 const val PINT = 0.125
 
 var playerDragonCoin = 5
-var playerGold = 10
-var playerSilver = 10
+var patronList = mutableListOf("Eli","Mordoc","Sophie")
+var menuList = File("data/tavern-menu-items.txt").readText().split("\n")
+var lastName = listOf("Ironfoot","Fernsworth","Baggins")
+val uniquePatrons = mutableSetOf<String>()
 
 fun main(args:Array<String>) {
-    println(toDragonSpeak("aeiou"))
-    placeOrder("shandy,DRAGON'S BREATH :IT'S GOT WHAT ADVANTURES CRAVE,5.91")
-    placeOrder("elixir,Shirley's Temple,4.12")
+
+    displayMenu(menuList)
+    (0..9).forEach {
+        val first = patronList.shuffled().first()
+        val last = lastName.shuffled().first()
+        val name = "$first+$last"
+        uniquePatrons += name
+    }
+
+    var orderCount = 0
+    while(orderCount <= 9){
+        placeOrder(uniquePatrons.shuffled().first(),
+        menuList.shuffled().first())
+        orderCount++
+    }
+//
+//    patronList.forEachIndexed { index, s ->
+//        println("Good evening, $s your're #${index+1} in line")
+//        placeOrder(s,uniquePatrons.shuffled().first())
+//    }
 }
 
-private fun placeOrder(menu:String){
+private fun placeOrder(patronName:String, menu: String){
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
-    println("Madrigal speak with $tavernMaster about their order")
+    println("$patronName speak with $tavernMaster about their order")
 
     val (type,name,price) = menu.split(',')
 
-    val message = "Madrigal buys a $name ($type) for $price"
+    val message = "$patronName buys a $name ($type) for $price"
     println(message)
 
-    performPurchase(price.toDouble())
+//    performPurchase(price.toDouble())
 
     val phrase = if( name.contains("Dragon's Breath",true)){
-        "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name!!")} remaing Wine is ${decreaseWine()}"
+        "$patronName exclaims ${toDragonSpeak("Ah, delicious $name!!")} remaing Wine is ${decreaseWine()}"
     }else{
-        "Madrigal syas: Thanks for the $name."
+        "$patronName says: Thanks for the $name."
     }
     println(phrase)
 }
@@ -52,7 +71,8 @@ private fun toDragonSpeak(phrase:String)=phrase.replace(Regex("[aeiouAEIOU]")){
 
 fun performPurchase(price:Double){
     displayBalance()
-    val totalPurse = playerGold + (playerSilver/100.0)
+//    val totalPurse = playerGold + (playerSilver/100.0)
+    val totalPurse = playerDragonCoin*4.17
     println("Total purse: $totalPurse")
     println("Purchase item for $price")
 
@@ -64,15 +84,27 @@ fun performPurchase(price:Double){
 
     println("Remaining balance: ${"%.2f".format(remainingBalance)}")
 
-    val remainingGold = remainingBalance.toInt()
-    val remainingSilver = (remainingBalance%1*100).roundToInt()
-    playerGold = remainingGold
-    playerSilver = remainingSilver
     displayBalance()
 }
 
 private fun displayBalance() {
-    println("Player's purchase balance: Gold: $playerGold ,Silver: $playerSilver")
+    println("Player's purchase balance: DragonGold: $playerDragonCoin")
 }
 
+private fun displayMenu(menu:List<String>){
+    val welcome = "*** Welcome to Taernyl's Folly ***"
+    val lineWidth = welcome.length
+
+    println(welcome)
+    menu.forEach {
+        val items = it.split(',')
+        var showMesage:String
+        showMesage = items[1].padEnd(lineWidth-items[2].length,'*')+items[2]
+        println(showMesage)
+        showMesage = "~[${items[0]}]~"
+        showMesage = showMesage.padStart(showMesage.length+(lineWidth - "~[${items[0]}]~".length)/2).padEnd(lineWidth)
+        println(showMesage)
+    }
+
+}
 
